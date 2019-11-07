bfc90b9fe52ee3fd576bd7e26c0c231178098766
dario zubaray <dzubaray@gmail.com>
2019-11-07 19:22:20 -0300

    creacion de usuarios

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(public afAuth: AngularFireAuth, private router: Router) { }
 
   crearUsuario(nombre: string, email: string, password: string): void {
     console.log({nombre, email});
@@ -14,6 +15,7 @@ export class AuthService {
     this.afAuth.auth.createUserWithEmailAndPassword(email, password)
         .then(resp => {
           console.log(resp);
+          this.router.navigate(['/']);
         })
         .catch(console.error);
   }
