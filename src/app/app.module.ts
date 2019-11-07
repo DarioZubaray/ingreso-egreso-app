67fe097580d0eecf9fac222530faa627b7d2649a
dario.zubaray <dzubaray@imagosur.com>
2019-11-07 12:17:02 -0300

    pantalla de registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
@@ -28,6 +29,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
