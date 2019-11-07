5ea5557cbd1e4d697d28f313bbd7173bcf082957
dario zubaray <dzubaray@gmail.com>
2019-11-07 19:48:44 -0300

    Obtener información del usuario activo en Firebase

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 
+import * as firebase from 'firebase';
+
 import Swal from 'sweetalert2';
 
 @Injectable({
@@ -11,12 +13,16 @@ export class AuthService {
 
   constructor(public afAuth: AngularFireAuth, private router: Router) { }
 
+  initAuthListener() {
+    this.afAuth.authState.subscribe((fbUser: firebase.User) => {
+        console.log(fbUser);
+    });
+  }
+
   crearUsuario(nombre: string, email: string, password: string): void {
-    console.log({nombre, email});
 
     this.afAuth.auth.createUserWithEmailAndPassword(email, password)
         .then(resp => {
-          console.log(resp);
           this.router.navigate(['/']);
         })
         .catch(err => {
@@ -27,7 +33,6 @@ export class AuthService {
 
   login(email: string, password: string) {
     this.afAuth.auth.signInWithEmailAndPassword(email, password).then(resp => {
-      console.log(resp);
       this.router.navigate(['/']);
     })
     .catch(err => {
