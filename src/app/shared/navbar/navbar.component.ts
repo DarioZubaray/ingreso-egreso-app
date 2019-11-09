35fa0b2ecfe2735720ca2971f91d93aadf1a6187
dario zubaray <dzubaray@gmail.com>
2019-11-08 22:26:05 -0300

    colocando el nombre de usuario activo

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styles: []
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  username: string;
+  subs = new Subscription();
+
+  constructor(public store: Store<AppState>) { }
 
   ngOnInit() {
+    this.subs = this.store.select('auth').pipe(filter(auth => auth.user != null)).subscribe(auth => {
+      this.username = auth.user.nombre;
+    });
   }
 
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: []
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit , OnDestroy {
 
-  constructor(public authService: AuthService) { }
+  username: string;
+  subs = new Subscription();
+
+  constructor(public authService: AuthService, public store: Store<AppState>) { }
 
   ngOnInit() {
+    this.subs = this.store.select('auth')
+        .pipe(filter(auth => auth.user != null))
+        .subscribe(auth => {
+          this.username = auth.user.nombre;
+        });
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 
   logout() {
